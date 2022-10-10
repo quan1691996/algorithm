7b5ff95142264b6735c38a70071d3a8ba4c58ecc
ldquan1996 <luongdoanhquan1996@gmail.com>
2022-10-10 21:46:11 +0900

    heap sort

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -1,19 +1,25 @@
 """
-Selection Sort
+Heap Sort
 """
 import unittest
 
-
-def insertion_sort(array: list):
-    for step in range(len(array)):
-        min = step
-        for i in range(step+1,len(array)):
-            if array[min]>array[i]:
-                min = i
-        array[min], array[step] = array[step], array[min]
+from algorithm.data_structure.heap.max_heap import MaxHeap
 
 
-class TestSelectionSort(unittest.TestCase):
+def heap_sort(array: list):
+    max_heap: MaxHeap = MaxHeap(array, len(array))
+    for i in range(len(array) - 1, 0, -1):
+        max_heap.swap(0, i)
+        max_heap.size = max_heap.size - 1
+        max_heap.heapify(0)
+
+    max_heap.size = len(array)
+    out: list = [element for element in max_heap]
+    for i in range(len(array)):
+        array[i] = out[i]
+
+
+class TestHeapSort(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.integers = [6, 8, 10, 26, 9, 2, 40, 22, 5, 32, 3]
@@ -21,13 +27,13 @@ class TestSelectionSort(unittest.TestCase):
         self.floats = [5.2, 4.4, 6.8, 9.5, 0.7, 1.2, 3.2, 5.9]
 
     def test_handles_multiple_array_type_input(self):
-        insertion_sort(self.integers)
+        heap_sort(self.integers)
         self.assertListEqual(self.integers, [2, 3, 5, 6, 8, 9, 10, 22, 26, 32, 40])
-        insertion_sort(self.strings)
+        heap_sort(self.strings)
         self.assertListEqual(
             self.strings, ["assembly", "c", "cpp", "go", "python", "sql", "swift"]
         )
-        insertion_sort(self.floats)
+        heap_sort(self.floats)
         self.assertListEqual(self.floats, [0.7, 1.2, 3.2, 4.4, 5.2, 5.9, 6.8, 9.5])
 
 
